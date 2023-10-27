65955e439d997b2642b88475902d225434f0b4f9
vulg3 <100254440+vulg3@users.noreply.github.com>
2023-10-27 13:13:07 +0700

    done layout

--- app/src/main/java/com/example/tttn/fragment_Taisan.java
+++ app/src/main/java/com/example/tttn/fragment_Taisan.java
@@ -1,16 +1,15 @@
 package com.example.tttn;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
+public class fragment_Taisan extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,4 +37,3 @@ public class MainActivity extends AppCompatActivity {
         return data;
     }
 }
-
